639f4013e278942c930d438293d2b50f67745db6
Joseph Muratov <jmuratov98@gmx.com>
2020-10-19 15:16:09 -0400

    Added a basic landing page

--- cuisino/cuisino/urls.py
+++ cuisino/cuisino/urls.py
@@ -15,10 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from users import views as users_views
+from cuisino import views as home_views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', users_views.register),
+    path('', home_views.landing_page)
 ]
