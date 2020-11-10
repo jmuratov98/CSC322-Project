323ab92a2b143685c22b4d522d53b7f5ff22d0d9
Joseph Muratov <jmuratov98@gmx.com>
2020-11-09 20:27:22 -0500

    Some user and roles modifications

--- cuisino/users/admin.py
+++ cuisino/users/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from users.models import Roles, Users
+from users.models import Users, Employees, Customers
 
 # Register your models here.
-admin.site.register(Roles)
 admin.site.register(Users)
+admin.site.register(Employees)
+admin.site.register(Customers)
--- cuisino/users/models.py
+++ cuisino/users/models.py
@@ -1,9 +1,30 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+
+from uuid import uuid4
 
 # Create your models here.
-class Roles(models.Model):
-    # These are the different types of Roles
+# class Roles(models.Model):
+#     # These are the different types of Roles
+#     CUSTOMER = 1
+#     CHEF = 2
+#     DELIVERY_BOY = 3
+#     ADMIN = 4 # This is going to be the owner
+
+#     ROLE_CHOICES = [
+#         ( CUSTOMER, 'Customer' ),
+#         ( CHEF, 'Chef' ),
+#         ( DELIVERY_BOY, 'Delivery Boy' ),
+#         ( ADMIN, 'Admin' )
+#     ]
+
+#     id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
+
+#     def __str__(self):
+#         return self.get_id_display()
+
+class Users(AbstractUser):
     CUSTOMER = 1
     CHEF = 2
     DELIVERY_BOY = 3
@@ -16,11 +37,32 @@ class Roles(models.Model):
         ( ADMIN, 'Admin' )
     ]
 
-    id = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, primary_key=True)
-
-    def __str__(self):
-        return self.get_id_display()
-
-class Users(AbstractUser):
+    class Meta:
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+    
     id = models.AutoField(primary_key=True)
-    role = models.ManyToManyField(Roles)
+    role = models.PositiveSmallIntegerField(choices=ROLE_CHOICES, default=CUSTOMER)
+    messages = models.TextField(null=True, blank=True)
+
+    USERNAME_FIELD = 'username'
+
+    
+class Employees(Users):
+
+    class Meta:
+        verbose_name = 'employee'
+        verbose_name_plural = 'employees'
+
+    salary = models.DecimalField(max_digits=5, decimal_places=2)
+    demoted = models.BooleanField(default=False)
+
+class Customers(Users):
+    class Meta:
+        verbose_name = 'customer'
+        verbose_name_plural = 'customers'
+
+    warnings = models.IntegerField()
+    VIP = models.BooleanField(default=False)
+    demoted = models.BooleanField(default=False)
+    deposit = models.DecimalField(decimal_places=2, max_digits=5, default=0.00)
