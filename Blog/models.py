ecd31240320eaf5eceaffaaff64afc4fa164fbd7
MuradAle <65867559+MuradAle@users.noreply.github.com>
2020-12-10 05:46:48 -0500

    created new model for post

---
+++ Blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.utils import timezone
+from django.conf import settings
+
+class Post(models.Model):
+    STATUS_CHOICES = (
+        ('draft','Draft'),
+        ('published','Published'),
+    )
+    title = models.CharField(max_length=250, unique=True)
+    slug = models.SlugField(max_length=250, unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE ,related_name='blog_posts')
+    published = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=9, choices=STATUS_CHOICES, default=0)
+    def __str__(self):
+        return self.title
