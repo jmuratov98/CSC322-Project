6775ee316969046cdf761ac4451a25703de27e1a
Joseph Muratov <jmuratov98@gmx.com>
2020-12-11 12:12:11 -0500

    Fixed some syntax errors

--- cuisino/users/views.py
+++ cuisino/users/views.py
@@ -89,38 +89,40 @@ def user_login(request):
         return render(request, 'users/login.html', {})
 
 
-def taboo_censor(request)
+def taboo_censor(request):
     if __name__ == "__main__":
         profanity.load_censor_words()
 
-        text = #Once Blog Implemented, string of text that gets submitted will be scanned here
+        # text = #Once Blog Implemented, string of text that gets submitted will be scanned here
+        text = ''
 
         text = profanity.censor(text)       
 
 
 
 
-def taboo_checker(request)
+def taboo_checker(request):
     count = 0
     item = Users.objects.get(warnings)
     if __name__ == "__main__":
-        text = #Once Blog Implemented, string of text that gets submitted will be scanned here
+        # text = #Once Blog Implemented, string of text that gets submitted will be scanned here
+        text = ''
 
-        if profanity.contains_profanity(text)= True
+        if profanity.contains_profanity(text) == True:
             item.warnings += 1
             VIP_to_Reg()
             Reg_to_Rip()
             
-def VIP_to_Reg()
+def VIP_to_Reg():
     item = Users.objects.get(warnings,VIP)
-    if item.VIP = True and item.warnings = 2
+    if item.VIP == True and item.warnings == 2:
         item.VIP = False
         item.warnings = 0
 
 
-def Reg_to_Rip(id)
+def Reg_to_Rip(id):
     item = Users.objects.get(VIP,id,warnings)
-    if item.VIP = False and item.warnings = 3
+    if item.VIP == False and item.warnings == 3:
         Users.objects.filter(id=id).delete()
         return redirect ('logout/')
     
