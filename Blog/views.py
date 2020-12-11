1a54c338518851eeca8fdf1348add24b71b804b6
Aiden Ferrari <ferianiaiden@gmail.com>
2020-12-11 02:28:17 -0500

    started to connect blog model attributes to taboo functions

--- Blog/views.py
+++ Blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from .models import Post
-
+from models import Comment
 from .forms import CommentForm
 
 def list_of_post(request):
@@ -29,3 +29,31 @@ def add_comment(request, slug):
 	template = 'Blog/post/add_comment.html'
 	context = {'form': form}
 	return render(request, template, context)
+
+
+	def taboo_censor(request)
+		item = Comment.objects.get(post,body)
+		swag = Post.objects.get(title, content)
+    	if __name__ == "__main__":
+        	profanity.load_censor_words()
+
+        	postvar = item.post
+			postvar = profanity.censor(postvar)  
+			bodyvar = item.body
+			bodyvar = profanity.censor(bodyvar)     
+			titlevar = swag.title
+			titlevar = profanity.censor(titlevar)
+			contentvar = swag.content
+			contentvar = profanity.censor(contentvar)
+
+
+
+def taboo_checker(request)
+   
+    if __name__ == "__main__":
+        text = #Once Blog Implemented, string of text that gets submitted will be scanned here
+
+        if profanity.contains_profanity(text)= True
+            Users.warnings += 1
+    
+            
